99256789a0ac5da85ff6fb14c032ec088b62fc55
RIFAAA🦊 <311837256+rifakhirindhoisetiawan@users.noreply.github.com>
2026-09-03 00:40:57 +0700

    UP

---
+++ scripts/templates/menus-array.js
@@ -0,0 +1,702 @@
+const menus = [
+        {
+          name: "COFFEE LATTE PREMIX",
+          icon: "🧊",
+          cls: "g-a",
+          cat: "PREMIX",
+          link: "pages/item-coffee-latte-ice-premix.html",
+          plu: "443775",
+        },
+        
+        {
+          name: "VANILLA LATTE PREMIX",
+          icon: "🍦",
+          cls: "g-b",
+          cat: "PREMIX",
+          link: "pages/item-vanilla-latte-premix.html",
+          plu: "443790 / 443791",
+        },
+        
+        {
+          name: "SALTED CARAMEL LATTE PREMIX",
+          icon: "🍮",
+          cls: "g-c",
+          cat: "PREMIX",
+          link: "pages/item-salted-caramel-latte-premix.html",
+          plu: "443786 / 443787",
+        },
+        
+        {
+          name: "BUTTERSCOTCH LATTE PREMIX",
+          icon: "🧈",
+          cls: "g-d",
+          cat: "PREMIX",
+          link: "pages/item-butterscotch-latte-premix.html",
+          plu: "443788 / 443789",
+        },
+        
+        {
+          name: "SALTED BUTTERCORN LATTE PREMIX",
+          icon: "🌽",
+          cls: "g-e",
+          cat: "PREMIX",
+          link: "pages/item-salted-buttercorn-latte-premix.html",
+          plu: "466231 / 466232",
+        },
+        
+        {
+          name: "JADI KEREN - KOPSU GULA AREN PREMIX",
+          icon: "😎",
+          cls: "g-f",
+          cat: "PREMIX",
+          link: "pages/item-jadi-keren-kopsu-gula-aren-premix.html",
+          plu: "443779 / 443780",
+        },
+        
+        {
+          name: "MOCHA LATTE PREMIX",
+          icon: "🍫",
+          cls: "g-g",
+          cat: "PREMIX",
+          link: "pages/item-mocha-latte-premix.html",
+          plu: "443783 / 443784",
+        },
+        
+        {
+          name: "JADI RINDU - KOPSU MADU PREMIX",
+          icon: "❤️",
+          cls: "g-h",
+          cat: "PREMIX",
+          link: "pages/item-jadi-rindu-kopsu-madu-premix.html",
+          plu: "443781 / 443782",
+        },
+        
+        {
+          name: "JADI MACCHIATTO PREMIX",
+          icon: "🍯",
+          cls: "g-a",
+          cat: "PREMIX",
+          link: "pages/item-jadi-macchiatto-premix.html",
+          plu: "466229 / 466230",
+        },
+        
+        {
+          name: "SALTED BUTTERCORN LATTE MANUAL",
+          icon: "🌽",
+          cls: "g-b",
+          cat: "MANUAL",
+          link: "pages/item-salted-buttercorn-latte-manual.html",
+          plu: "466231 / 466232",
+        },
+        
+        {
+          name: "JADI KEREN - KOPSU GULA AREN MANUAL",
+          icon: "😎",
+          cls: "g-c",
+          cat: "MANUAL",
+          link: "pages/item-jadi-keren-kopsu-gula-aren-manual.html",
+          plu: "443780",
+        },
+        
+        {
+          name: "JADI RINDU - KOPSU MADU MANUAL",
+          icon: "❤️",
+          cls: "g-d",
+          cat: "MANUAL",
+          link: "pages/item-jadi-rindu-kopsu-madu-manual.html",
+          plu: "443782",
+        },
+        
+        {
+          name: "MOCHA LATTE MANUAL",
+          icon: "🍫",
+          cls: "g-e",
+          cat: "MANUAL",
+          link: "pages/item-mocha-latte-manual.html",
+          plu: "443783 / 443784",
+        },
+        
+        {
+          name: "VANILLA LATTE MANUAL",
+          icon: "🍦",
+          cls: "g-f",
+          cat: "MANUAL",
+          link: "pages/item-vanilla-latte-manual.html",
+          plu: "443790 / 443791",
+        },
+        
+        {
+          name: "SALTED CARAMEL LATTE MANUAL",
+          icon: "🍮",
+          cls: "g-g",
+          cat: "MANUAL",
+          link: "pages/item-salted-caramel-latte-manual.html",
+          plu: "443786 / 443787",
+        },
+        
+        {
+          name: "BUTTERSCOTCH LATTE MANUAL",
+          icon: "🧈",
+          cls: "g-h",
+          cat: "MANUAL",
+          link: "pages/item-butterscotch-latte-manual.html",
+          plu: "443788 / 443789",
+        },
+        
+        {
+          name: "JA~DI MACCHIATTO MANUAL",
+          icon: "🍯",
+          cls: "g-a",
+          cat: "MANUAL",
+          link: "pages/item-jadi-macchiatto.html",
+          plu: "466230",
+        },
+        
+        {
+          name: "COFFEE LATTE ICE MANUAL",
+          icon: "🧊",
+          cls: "g-b",
+          cat: "MANUAL",
+          link: "pages/item-coffee-latte-ice-manual.html",
+          plu: "443776 / 443835",
+        },
+        
+        {
+          name: "CAPPUCCINO ICE",
+          icon: "🥛",
+          cls: "g-c",
+          cat: "MANUAL",
+          link: "pages/item-cappuccino-ice.html",
+          plu: "452795 / 452796",
+        },
+        
+        {
+          name: "AMERICANO HOT",
+          icon: "☕",
+          cls: "g-d",
+          cat: "MANUAL",
+          link: "pages/item-americano-hot.html",
+        },
+        
+        {
+          name: "AMERICANO ICE",
+          icon: "🧊",
+          cls: "g-e",
+          cat: "MANUAL",
+          link: "pages/item-americano-ice.html",
+        },
+        
+        {
+          name: "BLACK COFFEE WITH HONEY",
+          icon: "🍯",
+          cls: "g-f",
+          cat: "MANUAL",
+          link: "pages/item-black-coffee-with-honey.html",
+          plu: "443772",
+        },
+        
+        {
+          name: "HAWAIIAN AMERICANO (ICE UPSIZE)",
+          icon: "🌺",
+          cls: "g-g",
+          cat: "BLACK COFFEE",
+          link: "pages/item-hawaiian-americano-ice-upsize.html",
+        },
+        
+        {
+          name: "ESPRESSO HOT",
+          icon: "☕",
+          cls: "g-h",
+          cat: "ESPRESSO",
+          link: "pages/item-espresso-hot.html",
+          plu: "444571",
+        },
+        
+        {
+          name: "CAPPUCCINO HOT",
+          icon: "🫧",
+          cls: "g-a",
+          cat: "MANUAL",
+          link: "pages/item-cappuccino-hot.html",
+          plu: "445073",
+        },
+        
+        {
+          name: "CAFFELATTE HOT",
+          icon: "🥛",
+          cls: "g-b",
+          cat: "MANUAL",
+          link: "pages/item-caffelatte-hot.html",
+        },
+        
+        {
+          name: "HOT CHOCOLATE",
+          icon: "☕",
+          cls: "g-c",
+          cat: "CHOCO & MATCHA",
+          link: "pages/item-hot-chocolate.html",
+          plu: "443795",
+        },
+        
+        {
+          name: "ICE CHOCOLATE",
+          icon: "🧊",
+          cls: "g-d",
+          cat: "CHOCO & MATCHA",
+          link: "pages/item-ice-chocolate.html",
+          plu: "443910 / 443911",
+        },
+
+        
+        {
+          name: "ICE MATCHA",
+          icon: "🍃",
+          cls: "g-f",
+          cat: "CHOCO & MATCHA",
+          link: "pages/item-ice-matcha.html",
+          plu: "443792",
+        },
+        
+        {
+          name: "CHOCO BERRY (ICE 12OZ)",
+          icon: "🍓",
+          cls: "g-g",
+          cat: "CHOCO & MATCHA",
+          link: "pages/item-choco-berry-ice-12oz.html",
+          plu: "452794",
+        },
+        
+        {
+          name: "BLENDED VANILLA CARAMEL",
+          icon: "🍦",
+          cls: "g-h",
+          cat: "BLENDED",
+          link: "pages/item-blended-vanilla-caramel.html",
+        },
+        
+        {
+          name: "BLENDED CHOCO",
+          icon: "🍫",
+          cls: "g-a",
+          cat: "BLENDED",
+          link: "pages/item-blended-choco.html",
+        },
+        
+        {
+          name: "BLENDED CHOCO CARAMEL",
+          icon: "🍮",
+          cls: "g-b",
+          cat: "BLENDED",
+          link: "pages/item-blended-choco-caramel.html",
+          plu: "443793",
+        },
+        
+        {
+          name: "CREAMY COCONUT FRAPPE (UPSIZE)",
+          icon: "🥥",
+          cls: "g-c",
+          cat: "BLENDED",
+          link: "pages/item-creamy-coconut-frappe-upsize.html",
+        },
+        
+        {
+          name: "KOREAN STRAWBERRY MILK",
+          icon: "🍓",
+          cls: "g-d",
+          cat: "BLENDED",
+          link: "pages/item-korean-strawberry-milk.html",
+          plu: "443804 / 443805",
+        },
+        
+        {
+          name: "AFFOGATO",
+          icon: "🍨",
+          cls: "g-e",
+          cat: "BLENDED",
+          link: "pages/item-affogato.html",
+          plu: "466233",
+        },
+        
+        {
+          name: "ICE COLLAGEN LATTE (12OZ)",
+          icon: "🌸",
+          cls: "g-f",
+          cat: "BLENDED",
+          link: "pages/item-ice-collagen-latte-12oz.html",
+          plu: "453432",
+        },
+        
+        {
+          name: "ICE DOLCE LATTE (12OZ)",
+          icon: "🍬",
+          cls: "g-g",
+          cat: "BLENDED",
+          link: "pages/item-ice-dolce-latte-12oz.html",
+          plu: "453433",
+        },
+        
+        {
+          name: "COCONUT CLOUD LATTE",
+          icon: "🥥",
+          cls: "g-h",
+          cat: "BLENDED",
+          link: "pages/item-coconut-cloud-latte.html",
+          plu: "452799 / 452800",
+        },
+        
+        {
+          name: "PANDAN CLOUD LATTE",
+          icon: "🌿",
+          cls: "g-a",
+          cat: "BLENDED",
+          link: "pages/item-pandan-cloud-latte.html",
+          plu: "452797 / 452798",
+        },
+        
+        {
+          name: "SALTED CARAMEL COCA (12OZ)",
+          icon: "🥤",
+          cls: "g-b",
+          cat: "SOFT DRINK",
+          link: "pages/item-salted-caramel-coca-12oz.html",
+        },
+        
+        {
+          name: "TOFFEE POPCORN FANTA (12OZ)",
+          icon: "🍿",
+          cls: "g-c",
+          cat: "SOFT DRINK",
+          link: "pages/item-toffee-popcorn-fanta-12oz.html",
+        },
+        
+        {
+          name: "LYCHEE SPRITE (12OZ)",
+          icon: "🍋",
+          cls: "g-d",
+          cat: "SOFT DRINK",
+          link: "pages/item-lychee-sprite-12oz.html",
+        },
+        
+        {
+          name: "SHAKEN LYCHEE TEA",
+          icon: "🫐",
+          cls: "g-e",
+          cat: "JUS",
+          link: "pages/item-shaken-lychee-tea.html",
+          plu: "443806 / 443807",
+        },
+        
+        {
+          name: "SHAKEN HONEY LEMON TEA",
+          icon: "🍋",
+          cls: "g-f",
+          cat: "JUS",
+          link: "pages/item-shaken-honey-lemon-tea.html",
+          plu: "443808 / 443809",
+        },
+        
+        {
+          name: "GREEN JUICE",
+          icon: "🥬",
+          cls: "g-g",
+          cat: "JUS",
+          link: "pages/item-green-juice.html",
+        },
+        
+        {
+          name: "JUICE SIRSAK",
+          icon: "🍈",
+          cls: "g-h",
+          cat: "JUS",
+          link: "pages/item-juice-sirsak.html",
+          plu: "443842",
+        },
+        
+        {
+          name: "JUICE ALPUKAT",
+          icon: "🥑",
+          cls: "g-a",
+          cat: "JUS",
+          link: "pages/item-juice-alpukat.html",
+          plu: "443844",
+        },
+        
+        {
+          name: "JUICE BUAH NAGA",
+          icon: "🐲",
+          cls: "g-b",
+          cat: "JUS",
+          link: "pages/item-juice-buah-naga.html",
+          plu: "443845",
+        },
+        
+        {
+          name: "JUICE JAMBU",
+          icon: "🍒",
+          cls: "g-c",
+          cat: "JUS",
+          link: "pages/item-juice-jambu.html",
+          plu: "443843",
+        },
+        
+        {
+          name: "JUICE SEMANGKA",
+          icon: "🍉",
+          cls: "g-d",
+          cat: "JUS",
+          link: "pages/item-juice-semangka.html",
+          plu: "443838",
+        },
+        
+        {
+          name: "JUICE MELON",
+          icon: "🍈",
+          cls: "g-e",
+          cat: "JUS",
+          link: "pages/item-juice-melon.html",
+          plu: "443839",
+        },
+        
+        {
+          name: "JUICE PISANG STRAWBERRY",
+          icon: "🍌",
+          cls: "g-f",
+          cat: "JUS",
+          link: "pages/item-juice-pisang-strawberry.html",
+          plu: "444987",
+        },
+        
+        {
+          name: "JUICE MANGGA HARUM MANIS",
+          icon: "🥭",
+          cls: "g-g",
+          cat: "JUS",
+          link: "pages/item-juice-mangga-harum-manis.html",
+          plu: "446322",
+        },
+        
+        {
+          name: "JUICE APEL MIX",
+          icon: "🍎",
+          cls: "g-h",
+          cat: "JUS",
+          link: "pages/item-juice-apel-mix.html",
+        },
+        
+        {
+          name: "JUICE CARROT MIXED FRUIT",
+          icon: "🥕",
+          cls: "g-a",
+          cat: "JUS",
+          link: "pages/item-juice-carrot-mixed-fruit.html",
+          plu: "446279",
+        },
+        
+        {
+          name: "JUICE JERUK",
+          icon: "🍊",
+          cls: "g-b",
+          cat: "JUS",
+          link: "pages/item-juice-jeruk.html",
+          plu: "443847",
+        },
+        
+        {
+          name: "JUICE JERUK NAVEL",
+          icon: "🍊",
+          cls: "g-c",
+          cat: "JUS",
+          link: "pages/item-juice-jeruk-navel.html",
+        },
+        
+        {
+          name: "JUICE JERUK NAVEL CARA CARA",
+          icon: "🍊",
+          cls: "g-d",
+          cat: "JUS",
+          link: "pages/item-juice-jeruk-navel-cara-cara.html",
+        },
+        
+        {
+          name: "KOLAK LATTE",
+          icon: "🍮",
+          cls: "g-e",
+          cat: "BLENDED",
+          link: "pages/item-kolak-latte.html",
+          plu: "465840 / 465855",
+        },
+        
+        {
+          name: "WATERMELON COOLER",
+          icon: "🍉",
+          cls: "g-f",
+          cat: "BLENDED",
+          link: "pages/item-watermelon-cooler.html",
+          plu: "465844 / 465845",
+        },
+        
+        {
+          name: "PISANG IJO LATTE",
+          icon: "🍌",
+          cls: "g-g",
+          cat: "BLENDED",
+          link: "pages/item-pisang-ijo-latte.html",
+          plu: "465842 / 465843",
+        },
+        
+        { name: "PLU JA-DI", icon: "📋", cls: "g-e", cat: "UTILITY", link: "plu-ja-di.html" },
+        
+        { name: "PLANO", icon: "🗄️", cls: "g-h",
+          cat: "plano.html", link: "" },
+        
+        {
+          name: "TIME KAMERA",
+          icon: "📷",
+          cls: "g-c",
+          cat: "time-kamera.html",
+          link: "",
+        },
+        
+        {
+          name: "COFFEE LATTE",
+          icon: "☕",
+          cls: "g-a",
+          cat: "SPECIALTY & CLOUD",
+          link: "pages/item-coffee-latte.html",
+        },
+        
+        {
+          name: "CHOCO BERRY",
+          icon: "🍓",
+          cls: "g-g",
+          cat: "CHOCO & MATCHA",
+          link: "pages/item-choco-berry.html",
+        },
+        
+        {
+          name: "GREEN GRAPE FRUITYCANO",
+          icon: "🍇",
+          cls: "g-e",
+          cat: "SPECIALTY & CLOUD",
+          link: "pages/item-green-grape-fruitycano.html",
+        },
+        
+        {
+          name: "TROPICAL APPLE AMERICANO",
+          icon: "🍎",
+          cls: "g-g",
+          cat: "BLACK COFFEE",
+          link: "pages/item-tropical-apple-americano.html",
+        },
+        
+        {
+          name: "KOPI BOTOL JADI ASIK",
+          icon: "🍾",
+          cls: "g-e",
+          cat: "SPECIALTY & CLOUD",
+          link: "pages/item-kopi-botol-jadi-asik.html",
+          plu: "453434",
+        },
+        
+        {
+          name: "KOPI BOTOL JADI SERU",
+          icon: "🍾",
+          cls: "g-c",
+          cat: "SPECIALTY & CLOUD",
+          link: "pages/item-kopi-botol-jadi-seru.html",
+          plu: "453436",
+        },
+        
+        {
+          name: "KOPI BOTOL JADI SUKA",
+          icon: "🍾",
+          cls: "g-h",
+          cat: "SPECIALTY & CLOUD",
+          link: "pages/item-kopi-botol-jadi-suka.html",
+          plu: "453435",
+        },
+        
+        {
+          name: "CREAMY COCONUT FRAPPE",
+          icon: "🥥",
+          cls: "g-d",
+          cat: "BLENDED",
+          link: "pages/item-creamy-coconut-frappe.html",
+          plu: "451306",
+        },
+        
+        {
+          name: "PINK BERRY SODA",
+          icon: "🍒",
+          cls: "g-g",
+          cat: "SPECIALTY & CLOUD",
+          link: "pages/item-pink-berry-soda.html",
+          plu: "460953",
+        },
+        
+        {
+          name: "HAWAIIAN AMERICANO",
+          icon: "🌺",
+          cls: "g-c",
+          cat: "BLACK COFFEE",
+          link: "pages/item-hawaiian-americano.html",
+          plu: "451305",
+        },
+        
+        {
+          name: "ICE COLLAGEN LATTE",
+          icon: "🌸",
+          cls: "g-h",
+          cat: "SPECIALTY & CLOUD",
+          link: "pages/item-ice-collagen-latte.html",
+          plu: "453432",
+        },
+        
+        {
+          name: "MANGO COLADA",
+          icon: "🥭",
+          cls: "g-f",
+          cat: "SPECIALTY & CLOUD",
+          link: "pages/item-mango-colada.html",
+          plu: "460958",
+        },
+        
+        {
+          name: "CLOUDY LYCHEE",
+          icon: "🫐",
+          cls: "g-d",
+          cat: "SPECIALTY & CLOUD",
+          link: "pages/item-cloudy-lychee.html",
+          plu: "452163",
+        },
+        
+        {
+          name: "COLA CARAMELA",
+          icon: "🥤",
+          cls: "g-a",
+          cat: "SPECIALTY & CLOUD",
+          link: "pages/item-cola-caramela.html",
+          plu: "452161",
+        },
+        
+        {
+          name: "POPPY BERRY",
+          icon: "🌸",
+          cls: "g-h",
+          cat: "SPECIALTY & CLOUD",
+          link: "pages/item-poppy-berry.html",
+          plu: "452162",
+        },
+        
+        {
+          name: "MACCHIATO CLOUD FOAM",
+          icon: "☁️",
+          cls: "g-b",
+          cat: "SPECIALTY & CLOUD",
+          link: "pages/item-macchiato-cloud-foam.html",
+        },
+      ]+/g, '-').replace(/^-+|-+$/g, '');
+      }
+
+      function loadMenuPhotos() {
+        const exts = ['.jpg', '.jpeg', '.png', '.svg', '.webp'];
